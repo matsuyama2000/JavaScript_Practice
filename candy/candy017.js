9a0037248d969ddf33d0d41a6327e7ef18aad04b
Emily Ko <emily20000222@keio.jp>
2023-04-21 23:46:21 +0800

    add candy017 solution

--- candy/candy017.js
+++ candy/candy017.js
@@ -5,8 +5,15 @@
 
 function countBits(num) {
   // 實作寫在這裡
+  // the output binaryNumber is a string
+  const binaryNumber = num.toString(2);
+  // count the occurrences of the number 1
+  return (count = Array.from(binaryNumber).reduce(
+    (acc, cur) => (cur === "1" ? acc + 1 : acc),
+    0
+  ));
 }
 
-console.log(countBits(1234)) // 5
-console.log(countBits(1450)) // 6
-console.log(countBits(9527)) // 8
+console.log(countBits(1234)); // 5
+console.log(countBits(1450)); // 6
+console.log(countBits(9527)); // 8
